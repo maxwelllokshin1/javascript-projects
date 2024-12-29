d8c39c1d257073f1e37932dcd42dc55e72262993
Maxwell Lokshin <139578323+maxwelllokshin1@users.noreply.github.com>
2024-12-28 23:59:51 -0500

    Delete website/fbGame.js

--- website/fbGame.js
+++
@@ -1,8 +0,0 @@
-const fbCanvas = document.getElementById('fbCanvas');
-const fbCtx = fbCanvas.getContext('2d');
-
-var fbGameButton = 'fbStart';
-
-document.getElementById(fbGameButton).addEventListener('click',function(){
-    resetFlappyBird(fbCanvas, fbCtx, fbGameButton);
-});
