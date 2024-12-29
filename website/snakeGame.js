b917776fdc45b508da5164e4ea7958226557b8eb
Maxwell Lokshin <139578323+maxwelllokshin1@users.noreply.github.com>
2024-12-29 00:00:25 -0500

    Delete website/snakeGame.js

--- website/snakeGame.js
+++
@@ -1,8 +0,0 @@
-const snakeGameCanvas = document.getElementById('snakeCanvas');
-const snakeGameContext = snakeGameCanvas.getContext('2d');
-
-var snakeGamePressStart = 'snakeStart';
-
-document.getElementById(snakeGamePressStart).addEventListener('click', function(){
-    resetSnake(snakeGameContext, snakeGameCanvas, snakeGamePressStart, 'NONE');
-});
